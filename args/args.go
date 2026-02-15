89295e65c574fefdbf594452975225254b2a5356
jmkraus <crossinger@web.de>
2026-02-15 10:05:59 +0100

    Align description and wording to README

--- args/args.go
+++ args/args.go
@@ -3,14 +3,14 @@ package args
 import "flag"
 
 var (
-	ConfigPath  = flag.String("config", "", "Path to configuration file")
-	HistoryFile = flag.String("history", "", "Name of history file to preload (from config dir)")
-	Quiet       = flag.Bool("quiet", false, "Suppress info messages")
-	Model       = flag.String("model", "", "Set model for the request")
-	ShowVersion = flag.Bool("version", false, "Print picochat version")
-	Image       = flag.String("image", "", "Path to an image file")
-	Output      = flag.String("output", "", "Output format of the LLM (plain, json, json-pretty, yaml)")
-	Format      = flag.String("format", "", "Path to a JSON schema for the LLM content")
+	ConfigPath  = flag.String("config", "", "Loads a configuration file")
+	HistoryFile = flag.String("history", "", "Loads a specific session")
+	Quiet       = flag.Bool("quiet", false, "Suppresses all app messages")
+	Model       = flag.String("model", "", "Overrides configured model")
+	ShowVersion = flag.Bool("version", false, "Shows the version and exits")
+	Image       = flag.String("image", "", "Sets a path to an image file")
+	Output      = flag.String("output", "", "Sets the response output format (plain, json, json-pretty, yaml)")
+	Format      = flag.String("format", "", "Sets the path to a JSON schema file")
 )
 
 func Parse() {
